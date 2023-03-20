9d42c95316fdac439c958c23c7e274abf194bcac
dstoneva <stoilova.denitsa@gmail.com>
2023-03-20 16:47:52 +0200

    Create and map cardsData

--- sections/AgencySelection/AgencySelection.jsx
+++ sections/AgencySelection/AgencySelection.jsx
@@ -11,6 +11,42 @@ import {
   StyledCardsContainer
 } from "./elements";
 
+const cardsData = [
+  {
+    heading: "Brief",
+    text: (
+      <div>
+        Complete <strong>brief writing or simple guidance</strong> on what to include, we&apos;ve got you covered
+      </div>
+    ),
+    imageSrc: "/img/icons/edit-tools.png",
+    imageAlt: "Tools icon",
+    imagePosition: "left"
+  },
+  {
+    heading: "Search",
+    text: (
+      <div>
+        In-depth agency search covering; <strong>criteria matching</strong>, door knocking and due dilligence vetting.
+      </div>
+    ),
+    imageSrc: "/img/icons/business.png",
+    imageAlt: "Business icon",
+    imagePosition: "left"
+  },
+  {
+    heading: "Pitch",
+    text: (
+      <div>
+        Coperhensive <strong>pitch management</strong>, including comms, diary management and pitch hosting.
+      </div>
+    ),
+    imageSrc: "/img/icons/meeting.png",
+    imageAlt: "Business icon",
+    imagePosition: "left"
+  }
+];
+
 export const AgencySelection = ({ image, title, description, ctaText, ...props }) => {
   return (
     <StyledContainer {...props}>
@@ -23,13 +59,18 @@ export const AgencySelection = ({ image, title, description, ctaText, ...props }
           <Image src={image.src} alt={image.alt} width={image.width} height={image.height} priority={true} />
         </StyledImageContainer>
         <StyledCardsContainer>
-        <Card
-          heading='Title'
-          text='Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor inci '
-          imageSrc="/img/edit-tools.png"
-          imageAlt="Tools icon"
-          imagePosition="left"
-        />
+          {cardsData.map((card, i) => {
+            return (
+              <Card
+                key={i}
+                heading={card.heading}
+                text={card.text}
+                imageSrc={card.imageSrc}
+                imageAlt={card.imageAlt}
+                imagePosition={card.imagePosition}
+              />
+            );
+          })}
         </StyledCardsContainer>
       </StyledSectionContentContainer>
     </StyledContainer>
